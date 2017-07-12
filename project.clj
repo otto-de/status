004ed8a8713c39a40973547a4c0235afb2f3f251
Carl Düvel <carl.duevel@otto.de>
2017-07-12 17:25:03 +0200

    prepare release

--- project.clj
+++ project.clj
@@ -1,9 +1,10 @@
-(defproject de.otto/status "0.1.2"
+(defproject de.otto/status "0.1.3-SNAPSHOT"
   :description "A simple library to aggregate status."
   :url "https://github.com/otto-de/status"
   :license { :name "Apache License 2.0"
              :url "http://www.apache.org/license/LICENSE-2.0.html"}
   :scm { :name "git"
  	       :url "https://github.com/otto-de/status"}
-  :signing {:gpg-key "B641136D"}
-  :dependencies [[org.clojure/clojure "1.6.0"]])
+  :profiles {:dev {:plugins [[lein-release/lein-release "1.0.9"]]}}
+  :lein-release {:deploy-via :clojars}
+  :dependencies [[org.clojure/clojure "1.8.0"]])
